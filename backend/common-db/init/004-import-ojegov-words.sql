1a36220beb22a07b6917a93c90e36051a518e25f
ilya.avdeev <avdeeff.ilya@gmail.com>
2025-07-22 18:17:22 +0300

    fixed some misrecognition artifacts

--- backend/common-db/init/004-import-ojegov-words.sql
+++ backend/common-db/init/004-import-ojegov-words.sql
@@ -1,3 +1,3 @@
 \connect words;
 
-COPY public.ojegov FROM '/ojegov-source/prepared_voc.txt' WITH ( FORMAT CSV, DELIMITER '#', HEADER false, ENCODING 'UTF8');
+COPY public.ojegov FROM '/ojegov-source/prepared_voc.txt' WITH ( FORMAT CSV, DELIMITER '#', HEADER false, ENCODING 'UTF8', QUOTE '`', ESCAPE '\');
