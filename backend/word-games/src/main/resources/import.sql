5b33cba55fa6ea546b3936c7df1680a9faf87c21
ilya.avdeev <avdeeff.ilya@gmail.com>
2025-08-04 23:03:09 +0300

    2 first tests

--- backend/word-games/src/main/resources/import.sql
+++ backend/word-games/src/main/resources/import.sql
@@ -5,7 +5,19 @@ insert into Ojegov (id, word, explanation) values(2, 'слива', 'какое-�
 insert into Ojegov (id, word, explanation) values(3, 'вышка', 'какое-то пояснение3');
 insert into Ojegov (id, word, explanation) values(4, 'пижон', 'какое-то пояснение4');
 insert into Ojegov (id, word, explanation) values(5, 'каблук', 'какое-то пояснение5');
+insert into Ojegov (id, word, explanation) values(6, 'яйцо', 'какое-то пояснение6');
+insert into Ojegov (id, word, explanation) values(7, 'купон', 'какое-то пояснение7');
+insert into Ojegov (id, word, explanation) values(8, 'слив…', 'какое-то пояснение8');
 
+
+insert into all_words (id, word) values(1, 'смола');
+insert into all_words (id, word) values(2, 'слива');
+insert into all_words (id, word) values(3, 'вышка');
+insert into all_words (id, word) values(4, 'пижон');
+insert into all_words (id, word) values(5, 'каблук');
+insert into all_words (id, word) values(6, 'яйцо');
+insert into all_words (id, word) values(7, 'купон');
+insert into all_words (id, word) values(8, 'слив…');
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
--- backend/word-games/src/test/kotlin/one/avdeev/AllWordsRepositoryTest.kt
+++ backend/word-games/src/test/kotlin/one/avdeev/AllWordsRepositoryTest.kt
@@ -2,19 +2,20 @@ package one.avdeev
 
 import io.quarkus.test.junit.QuarkusTest
 import jakarta.inject.Inject
+import one.avdeev.repository.AllWordsRepository
 import one.avdeev.repository.OjegovRepository
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
-class OjegovRepositoryTest {
+class AllWordsRepositoryTest {
 
     @Inject
-    lateinit var ojegovRepository: OjegovRepository
+    lateinit var allWordsRepository: AllWordsRepository
 
     @Test
     fun given_OjegovRepositoryContainsёWords_when_RequestWordSearchingByWordLength_then_WordIsFound () {
-        var foundWords = ojegovRepository.findWord(4, ArrayList<String>(), listOf("?", "?", "?", "?"), ArrayList<String>())
+        var foundWords = allWordsRepository.findWord(4, ArrayList<String>(), listOf("?", "?", "?", "?"), ArrayList<String>())
         Assertions.assertEquals(1, foundWords.count(), "Найдено не одно слово")
         Assertions.assertEquals("яйцо", foundWords.get(0).word, "Найденное слово НЕ яйцо")
     }
@@ -22,14 +23,14 @@ class OjegovRepositoryTest {
 
     @Test
     fun given_OjegovRepositoryContainsRequiredWords_when_RequestWordSearchingByAllExactLetters_then_WordIsFound () {
-        var foundWords = ojegovRepository.findWord(5, ArrayList<String>(), listOf("с", "м", "о", "л", "а"), ArrayList<String>())
+        var foundWords = allWordsRepository.findWord(5, ArrayList<String>(), listOf("с", "м", "о", "л", "а"), ArrayList<String>())
         Assertions.assertEquals(1, foundWords.count(), "Найдено не одно слово")
         Assertions.assertEquals("смола", foundWords.get(0).word, "Найденное слово НЕ смола")
     }
 
     @Test
     fun given_OjegovRepositoryContainsRequiredWords_when_RequestWordSearchingBySeveralExactLetters_then_2WordsFound () {
-        var foundWords = ojegovRepository.findWord(5, ArrayList<String>(), listOf("с", "?", "?", "?", "а"), ArrayList<String>())
+        var foundWords = allWordsRepository.findWord(5, ArrayList<String>(), listOf("с", "?", "?", "?", "а"), ArrayList<String>())
         Assertions.assertEquals(2, foundWords.count(), "Найдено не 2 слова")
         Assertions.assertTrue(foundWords.map{it.word}.contains("смола"), "Слова Смола в списке найденных нет")
         Assertions.assertTrue(foundWords.map{it.word}.contains("слива"), "Слова Слива в списке найденных нет")
@@ -37,7 +38,7 @@ class OjegovRepositoryTest {
 
     @Test
     fun given_OjegovRepositoryContainsWords_when_RequestWordSearchingByNonPresentLetters_then_AllRequestedWordsFound() {
-        var foundWords = ojegovRepository.findWord(5, listOf("я","ц"), listOf("?", "?", "?", "?", "?"), ArrayList<String>())
+        var foundWords = allWordsRepository.findWord(5, listOf("я","ц"), listOf("?", "?", "?", "?", "?"), ArrayList<String>())
         Assertions.assertEquals(5, foundWords.count(), "Ожидаемо количество найденных слов 5")
         Assertions.assertTrue(foundWords.map{it.word}.contains("смола"), "Слова Смола в списке найденных нет")
         Assertions.assertTrue(foundWords.map{it.word}.contains("слива"), "Слова Слива в списке найденных нет")
@@ -48,14 +49,14 @@ class OjegovRepositoryTest {
 
     @Test
     fun given_OjegovRepositoryContainsWords_when_RequestWordSearchingByAllParams_then_AllRequestedWordsFound() {
-        var foundWords = ojegovRepository.findWord(5, listOf("ы"), listOf("?", "?", "?", "?", "а"), listOf("?в???"))
+        var foundWords = allWordsRepository.findWord(5, listOf("ы"), listOf("?", "?", "?", "?", "а"), listOf("?в???"))
         Assertions.assertEquals(1, foundWords.count(), "Ожидаемо количество найденных слов 1")
         Assertions.assertTrue(foundWords.map{it.word}.contains("слива"), "Слова Слива в списке найденных нет")
     }
 
     @Test
     fun given_OjegovRepositoryContainsWords_when_RequestWordSearching_then_IgnoreWordsWithEllipsis() {
-        var foundWords = ojegovRepository.findWord(5, ArrayList<String>(), listOf("с", "л", "и", "в", "?"), ArrayList<String>())
+        var foundWords = allWordsRepository.findWord(5, ArrayList<String>(), listOf("с", "л", "и", "в", "?"), ArrayList<String>())
         Assertions.assertEquals(1, foundWords.count(), "Ожидаемо количество найденных слов 1")
         Assertions.assertTrue(foundWords.map{it.word}.contains("слива"), "Слова Слива в списке найденных нет")
     }
